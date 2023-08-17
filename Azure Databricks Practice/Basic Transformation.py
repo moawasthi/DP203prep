3975e4f1a10e3b04252ad8bf6c9672fc8b4b2a7b
mohit.awasthi04@gmail.com <mohit.awasthi04@gmail.com>
2023-08-17 08:25:04 +0000

    added merge command

--- Azure Databricks Practice/Basic Transformation.py
+++ Azure Databricks Practice/Basic Transformation.py
@@ -4,10 +4,15 @@
 # COMMAND ----------
 
 # select columns with Alias
-df = readfiles("csv", "true", "true",",", "dbfs:/FileStore/ticketdata/MOCK_DATA.csv" )
+df = readfiles("csv", "true", "true",",", "dbfs:/FileStore/demo/MOCK_DATA.csv" )
 df.printSchema()
 df = df.select(col("id").alias("Id"), col("first_name"), col("last_name") )
 display(df)
+
+dfSorted = df.select(col("id").alias("Id"), concat_ws(", ", col("first_name"), col("last_name")).alias("fullName") )
+dfSorted = dfSorted.sort(dfSorted.fullName)
+display(dfSorted)
+
 
 # add columns
 df = df.select(concat_ws(", ", col("first_name"), col("last_name")).alias("fullName"))
@@ -18,12 +23,25 @@ display(df)
 # sort columns
 df = df.sort(df.fullName.asc())
 display(df)
+ 
 
 #group by
 df = df.groupBy(substring(df.fullName, 1,1).alias("StartsWith")).count()
 display(df)
 
+
+
 # filter columns
 df = df.filter(df.StartsWith == "A")
 display(df)
+
+# merge two dataframes together
+display(df)
+display(dfSorted)
+df = df.union(dfSorted)
+
+display(df)
+
+
+
 
