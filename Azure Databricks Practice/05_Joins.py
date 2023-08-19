a264108fa40e5a14d251ca0f87a35b72ca91ab52
mohit.awasthi04@gmail.com <mohit.awasthi04@gmail.com>
2023-08-19 03:10:59 +0000

    join commit with groupby

--- Azure Databricks Practice/05_Joins.py
+++ Azure Databricks Practice/05_Joins.py
@@ -1,10 +1,24 @@
 # Databricks notebook source
 # generate 5 copies
-df_emp = spark.read.option("header", "true").csv("/FileStore/demo/Employee.csv")
-df_emp.printSchema()
-df_num = spark.read.option("header", "true").csv("/FileStore/demo/Num.csv")
+df_emp = spark.read.option("header", "true").csv("/FileStore/Customers.csv")
+#df_emp.printSchema()
+#df_num = spark.read.option("header", "true").csv("/FileStore/Num.csv")
 #display(df_num)
-df_num = df_num.filter( df_num.Num <=5 )
-display(df_num)
-df_copies = df_emp.crossJoin(df_num) 
-display(df_copies)
+#df_num = df_num.filter( df_num.Num <=5 )
+#display(df_num)
+#df_copies = df_emp.crossJoin(df_num) 
+#display(df_copies)
+
+df_cust = spark.read.option("header","true").csv("/FileStore/Customers.csv")
+df_sales = spark.read.option("header","true").option("inferSchema", "true").csv("/FileStore/Orders.csv")
+df_salesorder = spark.read.option("header", "true").option("inferSchema", "true").csv("/FileStore/OrderDetails.csv")
+
+#display(df_cust)
+#display(df_sales)
+#display(df_salesorder)
+
+df_orderqty = df_sales.join(df_salesorder, df_sales.orderid == df_salesorder.orderid, "inner")
+df_orderqty.printSchema()
+df_orderqty = df_orderqty.groupBy("custid").agg( sum("qty"))
+display(df_orderqty)
+
