a617cdac82744754c39cc822e9428c561352a131
moawasthi <moawasthi>
2024-02-25 07:49:39 +0000

    test

---
+++ databricks_notebooks/1.0 - RawToBronze.py
@@ -0,0 +1,8 @@
+# Databricks notebook source
+dbutils.widgets.text("FunctionalDomain","", "Functional Domain")
+dbutils.widgets.text("FunctionalSubDomain","","Functional Sub Domain") 
+dbutils.widgets.text("flowId","","flow Id")
+dbutils.widgets.text("Datasource","","Data Source")
+dbutils.widgets.text("Entity","")
+dbutils.widgets.text("Ingestion Type","")
+dbutils.widgets.text("SubEntity","")
