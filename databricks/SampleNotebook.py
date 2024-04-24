d24c5ab4ea672e6efdcb65b99c6d6dfaee15056e
moawasthi <moawasthi>
2024-04-24 17:03:05 +0000

    added databricks code

--- databricks/SampleNotebook.py
+++ databricks/SampleNotebook.py
@@ -1,2 +1,27 @@
 # Databricks notebook source
-dbutils.fs.ls('abfss://masterdata@sabicontosodev.dfs.core.windows.net/raw/')
+dbutils.widgets.text("FunctionalDomain","")
+dbutils.widgets.text("FunctionalSubDomain","")
+dbutils.widgets.text("FlowId","")
+dbutils.widgets.text("DataSource","")
+dbutils.widgets.text("Entity","")
+dbutils.widgets.text("IngestionType","")
+dbutils.widgets.text("SubEntity","")
+
+
+# COMMAND ----------
+
+functional_domain = dbutils.widgets.get("FunctionalDomain")
+functional_sub_domain = dbutils.widgets.get("FunctionalSubDomain")
+flow_id = dbutils.widgets.get("FlowId")
+data_source = dbutils.widgets.get("DataSource")
+entity = dbutils.widgets.get("Entity")
+ingestion_type = dbutils.widgets.get("IngestionType")
+sub_entity = dbutils.widgets.get("SubEntity")
+display(functional_domain)
+
+# COMMAND ----------
+
+try:
+    dbutils.fs.ls('abfss://masterdata@sabicontosodev.dfs.core.windows.net/raw/master/file/product/full/PRODUCT')
+except:
+    dbutils.notebook.exit('no files in raw path')
