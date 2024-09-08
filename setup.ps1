022c0e222d44aefb1fd22e49b82533632b0438cd
moawasthi <31694684+moawasthi@users.noreply.github.com>
2024-09-08 16:02:48 +0530

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -26,7 +26,7 @@ $resourceGroup = New-AzResourceGroup -Name $resourceGroupName -Location $locatio
 
 # Create a server with a system wide unique server name
 #$server = New-AzSqlServer -ResourceGroupName $resourceGroupName -ServerName $serverName -Location $location -SqlAdministratorCredentials $(New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $adminSqlLogin, $(ConvertTo-SecureString -String $password -AsPlainText -Force))
-
+#
 # Create a server firewall rule that allows access from the specified IP range
 #$serverFirewallRule = New-AzSqlServerFirewallRule -ResourceGroupName $resourceGroupName -ServerName $serverName -FirewallRuleName "AllowedIPs" -StartIpAddress $startIp -EndIpAddress $endIp
 
@@ -36,15 +36,18 @@ $resourceGroup = New-AzResourceGroup -Name $resourceGroupName -Location $locatio
 # Create a storage account
 $storageaccount = New-AzStorageAccount -ResourceGroupName $resourceGroupName -Name $STRGACCNAME -Type $TypeSTRG -Location $location
 
+
+$DataFactory = Set-AzDataFactoryV2 -ResourceGroupName $resourceGroupName -Location $location -Name $dataFactoryName
+
+New-AzDatabricksWorkspace -Name mydatabricksws -ResourceGroupName $resourceGroupName -Location $location -ManagedResourceGroupName databricks-group -Sku standard
+
+# Connect to your Azure subscription
+Connect-AzAccount
 # Create a context object using Azure AD credentials
 $ctx = New-AzStorageContext -StorageAccountName $STRGACCNAME -UseConnectedAccount
 
 # Approach 1: Create a container
 New-AzStorageContainer -Name $containerName -Context $ctx
-
-$DataFactory = Set-AzDataFactoryV2 -ResourceGroupName $resourceGroupName -Location $location -Name $dataFactoryName
-
-New-AzDatabricksWorkspace -Name mydatabricksws -ResourceGroupName $resourceGroupName -Location $location -ManagedResourceGroupName databricks-group -Sku standard
 
 # Clean up deployment 
 # Remove-AzResourceGroup -ResourceGroupName $resourceGroupName
