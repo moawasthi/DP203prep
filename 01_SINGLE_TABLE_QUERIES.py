4b272d55c5088ace0b944f5a17b15dd6e110c9c0
moawasthi <moawasthi>
2024-10-29 04:22:12 +0000

    Group By PRactice

--- 01_SINGLE_TABLE_QUERIES.py
+++ 01_SINGLE_TABLE_QUERIES.py
@@ -33,3 +33,27 @@ df_sales_orders_addingYear_filtered = df_sales_orders_addingYear.filter(col("cus
 
 df_sales_orders_groupBy_v2 = df_sales_orders_addingYear_filtered.groupBy("empid").agg(max("orderyear").alias("maxyear"))
 display(df_sales_orders_groupBy_v2)
+
+# COMMAND ----------
+
+from pyspark.sql.functions  import *
+from pyspark.sql.types import DateType
+from pyspark.sql import functions as F
+
+# in sales order, return empid, totalfreight, count of orders for custid 71
+sales_orders = spark.read.option("header", True).format("csv").load("dbfs:/FileStore/Orders.csv")
+sales_orders_filtered = sales_orders.where(sales_orders["custid"] ==  71)
+sales_orders_filtered_addYear = sales_orders_filtered.withColumn("orderyear", col("orderdate").cast(DateType()))
+sales_orders_agg = sales_orders_filtered_addYear.groupBy("empid", "orderyear").agg(sum("freight").alias("totalfreight"), count("orderid").alias("numorders") )
+display(sales_orders_agg)
+
+
+# COMMAND ----------
+
+# from sales orders fetch employee id, orderyear, distinct number of customers
+
+sales_orders = spark.read.option(["header", True, "inferSchema", True]).format("csv").load("dbfs:/FileStore/Orders.csv")
+
+# COMMAND ----------
+
+
