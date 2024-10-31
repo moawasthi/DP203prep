3889b75f73e56bfb2e7c6ea5589178052b3f0a20
moawasthi <moawasthi>
2024-10-31 03:53:11 +0000

    practice and cleanup

--- 01_SINGLE_TABLE_QUERIES.py
+++ 01_SINGLE_TABLE_QUERIES.py
@@ -8,13 +8,16 @@
 # COMMAND ----------
 
 # select all data from SalesOrders
-df_sales_order = spark.read.option("header",True).format("csv").load("dbfs:/FileStore/Orders.csv")
-df_sales_order.show()
+df_sales_order = spark.read.option("header",True)\
+                           .format("csv").load("dbfs:/FileStore/Orders.csv")
+display(df_sales_order)
 
 # COMMAND ----------
 
 # select data from SalesOrders where custid = 71
-df_sales_orders= spark.read.option("header", True).format("csv").load("dbfs:/FileStore/Orders.csv")
+df_sales_orders= spark.read.option("header", True)\
+                           .format("csv")\
+                           .load("dbfs:/FileStore/Orders.csv")
 df_sales_orders_filtered = df_sales_orders.where(df_sales_orders["custid"] == 71)
 display(df_sales_orders_filtered)
 
@@ -33,6 +36,25 @@ df_sales_orders_addingYear_filtered = df_sales_orders_addingYear.filter(col("cus
 
 df_sales_orders_groupBy_v2 = df_sales_orders_addingYear_filtered.groupBy("empid").agg(max("orderyear").alias("maxyear"))
 display(df_sales_orders_groupBy_v2)
+
+# COMMAND ----------
+
+# select for custid 71 from sales orders the orderyear (another approach)
+
+from pyspark.sql.functions import col, year, max
+
+df_sales_order = spark.read.option("header", True)\
+                           .option("inferSchema", True)\
+                           .format("csv")\
+                           .load("dbfs:/FileStore/Orders.csv")
+
+df_sales_order_agg = df_sales_order.filter(col("custid") == 71)\
+                                 .withColumn("orderyear", year("orderdate"))\
+                                 .groupBy("empid", "orderyear").agg(max("orderyear").alias("maxorderyear"))\
+                                 .select("empid", "orderyear")
+                                 
+
+display(df_sales_order_agg)
 
 # COMMAND ----------
 
@@ -59,10 +81,42 @@ sales_orders = spark.read.option("header", True)\
 
 sales_orders_orderyear = sales_orders.withColumn("orderyear", year("orderdate"))
 
-sales_agg_orderyear_num = sales_orders_orderyear.groupBy("empid", "orderyear").agg(countDistinct("custid").alias("numcust"))
+sales_orders_orderyear_filtered = sales_orders_orderyear.filter(col(custid) == 71)
+
+sales_agg_orderyear_num = sales_orders_orderyear_filtered.groupBy("empid", "orderyear").agg(countDistinct("custid").alias("numcust"))
 
 display(sales_agg_orderyear_num)
 
 # COMMAND ----------
+
+#for custid 71, provide all the empids, orderyear which are greater than 1
+from pyspark.sql.functions import col, year, count
+sales_orders = spark.read.option("header",True)\
+                         .option("inferSchema", True)\
+                         .format("csv")\
+                         .load("dbfs:/FileStore/Orders.csv")
+sales_orders_filtered = sales_orders.filter( col("custid") == 71)
+sales_orders_addColumn = sales_orders_filtered.withColumn("orderyear", year("orderdate"))
+
+sales_orders_agg = sales_orders_addColumn.groupBy("empid", "orderyear")\
+                                         .agg(count("empid").alias("countEmpId"))\
+                                         .filter(col("countEmpId") > 0)
+display(sales_orders_agg)
+
+# COMMAND ----------
+
+# give distinct empId and orderyear from Sales Orders table for custid = 71
+from pyspark.sql.functions import col, year
+sales_orders = spark.read.option("header", True)\
+                         .option("inferSchema", True)\
+                         .format("csv")\
+                         .load("dbfs:/FileStore/Orders.csv")
+
+sales_orders_distinct =  sales_orders.filter(col("custid")==71)\
+                         .withColumn("orderyear", year("orderdate"))\
+                         .select("empid", "orderyear")\
+                         .distinct()
+
+display(sales_orders_distinct)
 
 
